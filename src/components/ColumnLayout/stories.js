9ab3dcd19fe0a6db489491b4dbaa725ed92c99ce
Ernie Bello <ernie@bello.nyc>
2018-11-13 13:50:22 -0500

    add new story with 2 videos

--- src/components/ColumnLayout/stories.js
+++ src/components/ColumnLayout/stories.js
@@ -16,4 +16,16 @@ storiesOf('ColumnLayout', module)
       <p>world</p>
       <p>3rd column</p>
     </ColumnLayout>
+  ))
+  .add('with 2 videos', () => (
+    <ColumnLayout>
+      <video controls="" poster="//d166gs6ce8o9t3.cloudfront.net/Tenet_Corp.jpg">
+        <source src="//d166gs6ce8o9t3.cloudfront.net/Tenet_Corp.mp4" type="video/mp4" />
+        <source src="//d166gs6ce8o9t3.cloudfront.net/Tenet_Corp.webm" type="video/webm" />
+      </video>
+      <video controls="" poster="//d166gs6ce8o9t3.cloudfront.net/Tenet_Corp.jpg">
+        <source src="//d166gs6ce8o9t3.cloudfront.net/Tenet_Corp.mp4" type="video/mp4" />
+        <source src="//d166gs6ce8o9t3.cloudfront.net/Tenet_Corp.webm" type="video/webm" />
+      </video>
+    </ColumnLayout>
   ));
