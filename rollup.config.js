996e6c42540701b5bd153618e5e3635a2021014d
Larry Roth <lroth@tenetpartners.com>
2019-05-17 15:11:50 -0400

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import url from 'rollup-plugin-url';
 import pkg from './package.json';
 
+
 export default {
   input: 'src/index.js',
   output: [
